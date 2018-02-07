7b0ef3629685a194d3f204c232df9e208b3ec00f
chinmay60 <chinmayvinchurkar24@gmail.com>
2018-02-08 01:14:03 +0530

    Delete key.py

--- key.py
+++
@@ -1,5 +0,0 @@
-# Twitter application consumer keys and access tokens.
-consumer_key = 'ERTTYHTAx6P91fLzrTTvabh1I'
-consumer_secret = 'PAiFjEEM2EzD7AFiiOij3viOZeVK884zuixqc4oZvgRigHsNKE'
-access_token = '961295881982033920-fJYqNvx0IYDTcz4OodNuJrcT93pJANp'
-access_token_secret = '19ppgAk9k9jbEJsTBMbBFr97pxM6nYirJC7vQcsX0CBFR'
