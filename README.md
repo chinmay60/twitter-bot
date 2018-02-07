7fd34296f912b3c7fef7500cada2558073b19b2c
chinmay60 <chinmayvinchurkar24@gmail.com>
2018-02-08 01:13:44 +0530

    fixes

---
+++ README.md
@@ -0,0 +1,1 @@
+a twitter bot made using python
--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
-
-
 import tweepy
 from time import sleep
 
-from key import *
+consumer_key = 'ERTTYHTAx6P91fLzrTTvabh1I'
+consumer_secret = 'PAiFjEEM2EzD7AFiiOij3viOZeVK884zuixqc4oZvgRigHsNKE'
+access_token = '961295881982033920-fJYqNvx0IYDTcz4OodNuJrcT93pJANp'
+access_token_secret = '19ppgAk9k9jbEJsTBMbBFr97pxM6nYirJC7vQcsX0CBFR'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
