6bfbc408bc26e8902f58768fd8050e8f2f105690
chinmay <chinmayvinchurkar24@gmail.com>
2018-02-07 23:44:09 +0530

    made changes

--- bot.py
+++ bot.py
@@ -1,23 +1,26 @@
+
+
 import tweepy
 from time import sleep
+
 from key import *
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
+count = int(10000)
 
-count = 1
 
-for tweet in tweepy.Cursor(api.search, q='#funfact').items(1): #change #funfact to any other hashtag you want 
-    
+for tweet in tweepy.Cursor(api.search, q='#funfact').items(count):
     try:
+        print('\nRetweet Bot found tweet by @' + tweet.user.screen_name + '. ' + 'Attempting to retweet.')
 
         tweet.retweet()
-        print("count = "+count)
-        sleep = (3600)
-    
-    except tweepy.TweepError as error:
+        print('Retweet published successfully.')
+        count = int(10000)
+        sleep(3600)
 
+    except tweepy.TweepError as error:
         print('\nError. Retweet not successful. Reason: ')
         print(error.reason)
 
